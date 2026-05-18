661461c17b3f73fc22f1ef4bdc303917712ee8ed
saya parajuli <susayaparajuli@gmail.com>
2026-05-18 22:58:20 +0545

    added role in login response

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -61,5 +61,6 @@ class LoginSerializer(TokenObtainPairSerializer):
                 "email": user.email,
                 "first_name": user.first_name,
                 "last_name": user.last_name,
+                "role": user.role,
             },
         }
